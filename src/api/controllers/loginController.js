cdc34e8961bbb0beadc19877f7e62e3351ddf656
André Carmo <andre_de11@msn.com>
2021-08-21 16:16:04 -0300

    Implementing controller that manages the login request and token creation behavior.

--- src/api/controllers/loginController.js
+++ src/api/controllers/loginController.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
 const { validateLogin } = require("../middlewares/userValidations");
+const userServices = require("../services/userService");
 
 const router = Router();
 
 router.post("/", validateLogin, async (req, res) => {
     const { email, password } = req.body;
+    const token = await userServices.loginUser(email, password);
+    const result = token
+        ? { status: 200, body: { token } }
+        : { status: 401, body: { message: "Incorrect username or password" } };
+
+    res.status(result.status).json(result.body);
 });
 
 module.exports = router;
