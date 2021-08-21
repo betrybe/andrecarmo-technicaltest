8240a6f2e598b4171eb96671cd906912b3777057
André Carmo <andre_de11@msn.com>
2021-08-21 16:12:46 -0300

    implementing login service operation, which consists of creating a token if the user's email and password are correct.

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,4 +1,5 @@
 const users = require("../repositories/userRepository");
+const createToken = require("../authentication/createToken");
 
 const createUser = async (name, email, password) => {
     const user = await users.findByEmail(email);
@@ -8,4 +9,17 @@ const createUser = async (name, email, password) => {
     return newUser;
 };
 
-module.exports = { createUser };
+const loginUser = async (email, password) => {
+    const user = await users.findByEmailAndPassword(email, password);
+    if (!user) return null;
+
+    const payload = {
+        id: user._id,
+        email,
+        role: user.role,
+    };
+
+    const token = createToken(payload);
+    return token;
+};
+module.exports = { createUser, loginUser };
