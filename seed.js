5a29b81a4d67f99e8acfa7bce62106546a549432
André Carmo <andre_de11@msn.com>
2021-08-21 16:23:18 -0300

    Adding seed for recipes.

--- seed.js
+++ seed.js
@@ -5,3 +5,9 @@ db.users.insertOne({
     password: "12345678",
     role: "user",
 });
+
+db.recipes.insertOne({
+    name: "Receita do Jacquin",
+    ingredients: "Frango",
+    preparation: "10 minutos no forno",
+});
