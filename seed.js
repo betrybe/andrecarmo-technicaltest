8ac01ea747aef003ef059f1ca6c87ed0662e1329
André Carmo <andre_de11@msn.com>
2021-08-21 21:17:29 -0300

    Adjustments to make seed file as sixth requisite

--- seed.js
+++ seed.js
@@ -1,13 +1,1 @@
-// colocar query do MongoDB
-db.users.insertOne({
-    name: "Erick Jacquin",
-    email: "erickjacquin@gmail.com",
-    password: "12345678",
-    role: "user",
-});
-
-db.recipes.insertOne({
-    name: "Receita do Jacquin",
-    ingredients: "Frango",
-    preparation: "10 minutos no forno",
-});
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
