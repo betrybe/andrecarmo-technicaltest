e8286b4efd4a889e2a89aecf869c7dbbcb2de5c4
André Carmo <andre_de11@msn.com>
2021-08-22 10:41:13 -0300

    Adding route to access an uploaded recipe image.

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const userController = require("./controllers/userController");
 const loginController = require("./controllers/loginController");
@@ -16,5 +17,6 @@ app.get("/", (request, response) => {
 app.use("/users", userController);
 app.use("/login", loginController);
 app.use("/recipes", recipeController);
+app.use("/images/", express.static(path.join(__dirname, "../uploads")));
 
 module.exports = app;
